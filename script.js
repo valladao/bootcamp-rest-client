251df11f1651ed176629e3c2e00d79a2d711949d
valladao <valladao@gmail.com>
2021-06-13 12:45:01 -0300

    Fetch corrected bug

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const fetch = require("node-fetch")
 
+const data = JSON.stringify({ name: "Roger", age: 8 })
+
 ;(async () => {
   const response = await fetch("https://ptsv2.com/t/g7rl0-1623597417/post", {
     method: "POST",
@@ -7,11 +9,7 @@ const fetch = require("node-fetch")
       "content-type": "application/json",
       authorization: "Bearer 123abc456def",
     },
-    body: {
-      name: "Roger",
-      age: 8,
-    },
+    body: data,
   })
-
-  console.log(response.data)
+  console.log(await response.text())
 })()
