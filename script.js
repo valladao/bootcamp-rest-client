a2a3fb3a17728beeacc2f07a23148f40957de506
valladao <valladao@gmail.com>
2021-06-13 12:23:14 -0300

    XHR POST request

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 const xhr = new XMLHttpRequest()
 
+const data = JSON.stringify({
+  name: "Roger",
+  age: 8,
+})
+
+xhr.withCredentials = true
+
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === this.DONE) {
-    const breeds = JSON.parse(this.responseText).message
-    for (const breed in breeds) {
-      console.log(breed)
-    }
+    console.log(this.responseText)
   }
 })
 
-xhr.open("GET", "https://dog.ceo/api/breeds/list/all")
+xhr.open("POST", "https://ptsv2.com/t/g7rl0-1623597417/post")
+xhr.setRequestHeader("content-type", "application/json")
+xhr.setRequestHeader("authorization", "Bearer 123abc456def")
 
-xhr.send()
+xhr.send(data)
