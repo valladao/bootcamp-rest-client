92e828c3bcfcd7fd15ad29ddd469939eed646a32
valladao <valladao@gmail.com>
2021-06-13 11:23:27 -0300

    The Fetch API

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
-const xhr = new XMLHttpRequest()
+const fetch = require("node-fetch")
 
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === this.DONE) {
-    const breeds = JSON.parse(this.responseText).message
-    for (const breed in breeds) {
-      console.log(breed)
-    }
+;(async () => {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all", {
+    method: "GET",
+  })
+
+  const data = await response.json()
+  for (const breed in data.message) {
+    console.log(breed)
   }
-})
-
-xhr.open("GET", "https://dog.ceo/api/breeds/list/all")
-
-xhr.send()
+})()
