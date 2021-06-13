21d8e1bf4a39781225257e02a5527373521dcd5b
valladao <valladao@gmail.com>
2021-06-13 13:39:02 -0300

    request library POST request

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const request = require("request")
 
 const options = {
-  method: "GET",
-  url: "https://dog.ceo/api/breeds/list/all",
+  method: "POST",
+  url: "https://ptsv2.com/t/g7rl0-1623597417/post",
+  headers: {
+    "content-type": "application/json",
+    authorization: "Bearer 123abc456def",
+  },
+  body: { name: "Roger", age: 8 },
   json: true,
 }
 
 request(options, (error, response, body) => {
   if (error) throw new Error(error)
 
-  for (const breed in body.message) {
-    console.log(breed)
-  }
+  console.log(body)
 })
