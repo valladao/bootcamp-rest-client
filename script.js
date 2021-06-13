504e58f1f6f0f0717c5c93df873bfadf9fe50fcc
valladao <valladao@gmail.com>
2021-06-13 17:35:22 -0300

    Implement CORS in Express

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const axios = require("axios")
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    const requests = [
+      {
+        url: "https://flaviocopes-cors-example-express.glitch.me/without-cors",
+        container: "response-one",
+      },
+      {
+        url: "https://flaviocopes-cors-example-express.glitch.me/with-cors",
+        container: "response-two",
+      },
+    ]
 
-;(async () => {
-  const response = await axios.get("https://dog.ceo/api/breeds/list/all")
-  for (const breed in response.data.message) {
-    console.log(breed)
-  }
-})()
+    for (const request of requests) {
+      fetch(request.url)
+        .then((response) => {
+          response.json().then((data) => {
+            document
+              .getElementById(request.container)
+              .getElementsByTagName("p")[0].innerHTML = data.msg
+          })
+        })
+        .catch((err) => {
+          document
+            .getElementById(request.container)
+            .getElementsByTagName("p")[0].innerHTML = "CORS Error! 😯"
+        })
+    }
+  },
+  false
+)
