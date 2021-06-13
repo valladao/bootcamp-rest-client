9bf615198eeb6b4c33477f98456c1fe9bf864253
valladao <valladao@gmail.com>
2021-06-13 13:44:31 -0300

    Axios Post Request

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const axios = require("axios")
 
 ;(async () => {
-  const response = await axios.get("https://dog.ceo/api/breeds/list/all")
-  for (const breed in response.data.message) {
-    console.log(breed)
-  }
+  const response = await axios.post(
+    "https://ptsv2.com/t/g7rl0-1623597417/post",
+    {
+      name: "Roger",
+      age: 8,
+    },
+    {
+      headers: {
+        "content-type": "application/json",
+        authorization: "Bearer 123abc456def",
+      },
+    }
+  )
+
+  console.log(response.data)
 })()
